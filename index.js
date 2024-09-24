0e7c2cddd891f6c0a6d959fb0ca7c2a72a34eaf8
lmnleaf <lmnleaf@users.noreply.github.com>
2024-09-24 14:04:58 -0600

    updates action inputs

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 import * as github from '@actions/github';
 import * as core from '@actions/core';
-import * as dotenv from 'dotenv';
 import { artifactUsageReport } from './src/artifact-report.js';
 
-const env = dotenv.config();
-const token = process.env.GITHUB_TOKEN;
-const owner = process.env.OWNER;
-const repo = process.env.REPO;
+// Note: use when running locally
+// import * as dotenv from 'dotenv';
+// const env = dotenv.config();
+// const token = process.env.GITHUB_TOKEN;
+// const owner = process.env.OWNER;
+// const repo = process.env.REPO;
+// const path = './';
+
+const context = github.context;
 
 async function main() {
   try {
+    const token = core.getInput('token');
     const octokit = new github.getOctokit(token);
-    const currentPeriodDays = 7;
-    const path = './';
+    const currentPeriodDays = processInput(core.getInput('current_period_days'));
+    const path = core.getInput('path');
 
     await artifactUsageReport.createReport(currentPeriodDays, path, repo, owner, octokit);
+
+    return core.notice('Artifact usage report created.');
   } catch(error) {
-    console.log(error);
+    core.setFailed(error.message);
   }
 }
 
